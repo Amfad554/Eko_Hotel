25e0b8272521fa871c9a58f79f193ea746473417
Favour <georgechiamaka02@gmail.com>
2025-08-05 17:33:59 -0700

    Add folder

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+     colors:{
+       banner1:"rgb(4, 48, 64)",
+       Arival:"rgb(84, 85, 85)",
+       company:"rgb(205, 162, 116)"
+     }
+    },
+  },
+  plugins: [],
+ }
