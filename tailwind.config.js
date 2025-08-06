f16638198d2bcf9cd5299ada6dc9ccae6b076f3b
Favour <georgechiamaka02@gmail.com>
2025-08-05 21:35:00 -0700

    worked on banner4 and 5

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
      colors:{
        banner1:"rgb(4, 48, 64)",
        Arival:"rgb(84, 85, 85)",
-       company:"rgb(205, 162, 116)"
+       company:"rgb(205, 162, 116)",
+       ban5:"rgb(205, 162, 116)",
+       OUR:"rgb(4, 48, 64)",
+       image:"rgb(243, 233, 223)",
+       box:"rgb(4, 48, 63)",
+
      }
     },
   },
